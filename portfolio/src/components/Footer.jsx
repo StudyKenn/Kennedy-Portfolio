965177e31f3f3ad097203e97388150ae1ff7f89e
StudyKenn <149020445+StudyKenn@users.noreply.github.com>
2025-07-10 23:40:34 -0400

    now

--- portfolio/src/components/Footer.jsx
+++ portfolio/src/components/Footer.jsx
@@ -5,11 +5,11 @@ function Footer() {
     return (
         <footer className="footer">
             <ul>
-                <a href="https://github.com/yourusername" target="_blank" rel="noopener noreferrer">
+                <a href="https://github.com/StudyKenn" target="_blank" rel="noopener noreferrer">
                     GitHub
                 </a>{' '}
                 |{' '}
-                <a href="https://linkedin.com/in/yourusername" target="_blank" rel="noopener noreferrer">
+                <a href="https://www.linkedin.com/in/kennedy-amucha-1257ab294?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BzOHOEvmbRieIM2tMAb96IA%3D%3D" target="_blank" rel="noopener noreferrer">
                     LinkedIn
                 </a>
             </ul>
